b89a2ea025954a87c87f16a3f238b28310dec108
Liam Gomez <liamgomez@nevada.unr.edu>
2017-09-25 18:58:35 -0700

    Initial commit

---
+++ app/src/main/java/com/liamgomez/cs454_shake_unlock/UnlockActivity.java
@@ -0,0 +1,13 @@
+package com.liamgomez.cs454_shake_unlock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UnlockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unlock);
+    }
+}
